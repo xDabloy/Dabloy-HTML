6be4bd5c43ccfb6bf20647207ca1b11021eab600
Uczen <uczen@dzdzw.local>
2025-09-19 12:57:43 +0200

    dodano zadanie 19.09

---
+++ Zadanie 19.09.25r/meteo.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl4.css">
+    <title>Prognoza pogody Poznań</title>
+</head>
+
+<body>
+    <div class="container_up">
+        <div class="gora_lewo">
+            <p> maj, 2019r</p>
+        </div>
+        <div class="gora_srodek">
+            <h2> Prognoza dla Poznania</h2>
+        </div>
+        <div class="gora_prawo">
+            <img src="logo.png" alt="Prognoza">
+        </div>
+    </div>
+    <div class="container_mid">
+        <div class="mid_lewy">
+            <p>
+                <a href="kwerendy.txt">Kwerendy</a>
+            </p>
+        </div>
+        <div class="mid_prawy"> <img src="obraz.jpg" alt="Polska, Poznań"></div>
+    </div>
+
+    <div class="glowny">
+        <table>
+        <tr>
+            <th>Lp</th>
+            <th>DATA</th>
+            <th>NOC - TEMPERATURA</th>
+            <th>DZIEŃ - TEMPERATURA</th>
+            <th>OPADY [mm/h]</th>
+            <th>CIŚNIENIE [hPa]</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>2019-05-18</td>
+            <td>12</td>
+            <td>15</td>
+            <td>30</td>
+            <td>996</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2019-05-17</td>
+            <td>11</td>
+            <td>15</td>
+            <td>30</td>
+            <td>995</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>2019-05-16</td>
+            <td>11</td>
+            <td>17</td>
+            <td>30</td>
+            <td>995</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>2019-05-15</td>
+            <td>8</td>
+            <td>19</td>
+            <td>4</td>
+            <td>1000</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>2019-05-14</td>
+            <td>8</td>
+            <td>23</td>
+            <td>4</td>
+            <td>1000</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>2019-05-13</td>
+            <td>5</td>
+            <td>20</td>
+            <td>0</td>
+            <td>1020</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>2019-05-12</td>
+            <td>5</td>
+            <td>20</td>
+            <td>0</td>
+            <td>1020</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>2019-05-11</td>
+            <td>11</td>
+            <td>23</td>
+            <td>0</td>
+            <td>1020</td>
+        </tr>
+        </table>
+    </div>
+
+    <div class="stopka">
+        <p>Stronę wykonal: 000000000000</p>
+    </div>
+</body>
+
+</html>
